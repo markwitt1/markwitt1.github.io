bd38e1b531861a195470a8afa762904e0143aa37
Mark Witt <mwitt@newstore.com>
2024-02-10 14:03:02 +0100

    Remove store

--- src/content/config.ts
+++ src/content/config.ts
@@ -14,26 +14,10 @@ const blogSchema = z.object({
     .optional(),
 });
 
-const storeSchema = z.object({
-  title: z.string(),
-  description: z.string(),
-  custom_link_label: z.string(),
-  custom_link: z.string().optional(),
-  updatedDate: z.coerce.date(),
-  pricing: z.string().optional(),
-  oldPricing: z.string().optional(),
-  badge: z.string().optional(),
-  checkoutUrl: z.string().optional(),
-  heroImage: z.string().optional(),
-});
-
 export type BlogSchema = z.infer<typeof blogSchema>;
-export type StoreSchema = z.infer<typeof storeSchema>;
 
 const blogCollection = defineCollection({ schema: blogSchema });
-const storeCollection = defineCollection({ schema: storeSchema });
 
 export const collections = {
   blog: blogCollection,
-  store: storeCollection,
 };
