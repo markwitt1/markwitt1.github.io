3f8f4792175f9d4650e9d4918d26d7f690a31106
Mark Witt <mwitt@newstore.com>
2024-02-10 13:55:06 +0100

    Add own domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://markwitt1.github.io",
-  base: "/",
+  site: "https://markwitt.me",
   integrations: [mdx(), sitemap(), tailwind()],
 });
